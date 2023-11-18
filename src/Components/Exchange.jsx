0741282fc53bc38c835c107f74edecc1a625f3d1
Ayush Singh <ayushsinghmi711@gmail.com>
2023-11-18 16:15:01 +0530

    token-box made

--- src/Components/Exchange.jsx
+++ src/Components/Exchange.jsx
@@ -3,23 +3,53 @@ import Wallet from './wallet'
 
 function Exchange() {
   return (
-    <div className=' w-full h-full flex justify-center items-center'>
+    <div className=' w-full h-screen flex justify-center items-center'>
     <div className=' h-[600px] w-[600px] bg-blue-200
    mt-40 border rounded-2xl'>
       <div className='flex px-20 py-5 mb-[10px]'>
         <h1 className='mr-10 text-xl font-semibold text-gray-900'>Swap</h1>
         <h2 className='text-xl font-semibold text-gray-900'>Bridge</h2>
       </div>
+
       <div className='h-[150px] w-[400px] bg-blue-300 mx-auto border
-      rounded-2xl'>
+      rounded-2xl flex flex-col'>
+      <div className=' h-[60px] w-[340px] mx-auto mt-[10px]
+      text-xl py-[5px]'>
+      send</div>
+      <div className='bg-red-300 h-[60px] w-[340px] mx-auto
+      mt-[10px] font-semibold text-4xl flex justify-between'>
+       <input className='bg-blue-300 h-[40px] w-[40px] mt-[5px]
+      ml-[5px]'
+      placeholder='0' type='text'  value='0'></input>
+      <div className='bg-gray-300 h-[40px] w-[100px] text-sm
+      flex justify-center items-center border rounded-xl mt-[10px] mr-[10px]' >
+       polygon
+      </div>
+      </div>
  </div>
+
  <div className='h-[150px] w-[400px] bg-blue-300 mx-auto border
       mt-[20px] rounded-2xl'>
+        <div className=' h-[60px] w-[340px] mx-auto mt-[10px]
+      text-xl py-[10px]'>
+      recieve</div>
+      <div className='bg-red-300 h-[60px] w-[340px] mx-auto
+      mt-[10px] font-semibold text-4xl flex  justify-between'>
+      <input className='bg-blue-300 h-[40px] w-[40px] mt-[5px]
+      ml-[5px]'
+      placeholder='0' type='text'  value='0'></input>
+      <div className='bg-gray-300 h-[40px] w-[100px] text-sm
+      flex justify-center items-center border rounded-xl mt-[10px] mr-[10px]' >
+       polygon
+      </div>
+      </div> 
  </div>
+
     <div className='mx-auto w-[200px] h-[100px] mt-[20px] bg-transparent
      rounded-2xl flex items-center justify-center'>
       <Wallet/>
     </div>
+
     <div className='mx-auto mt-[20px] h-[40px] bg:transparent w-[400px]
     flex items-center justify-center  rounded-2xl'>
       <h1 className='text-2xl tracking-wide'>Powered by 
